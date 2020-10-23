18e2d5ad13325cd3d068b6a3b0783e199b986493
ZeroDestru <69701102+ZeroDestru@users.noreply.github.com>
2020-10-23 20:47:35 -0300

    Add files via upload

---
+++ js/produto.js
@@ -0,0 +1,169 @@
+async function listarProdutos() {
+    let resposta = await sendRequest("/api/produto");
+    // fetch(urlApi + "/api/categoria").then(function(resposta){ });
+    let json = await resposta.json();
+
+    let table = document.getElementById("produtos");
+
+    if (table == null) return;
+
+    let tbody = table.getElementsByTagName("tbody")[0];
+
+    if (tbody == null) return;
+
+    tbody.innerHTML = "";
+
+    json.forEach(produto => {
+        let linha = document.createElement("tr"); //<tr></tr>
+
+        addColumn("id", produto, linha);
+        let colunaImagem = document.createElement("td");
+        if (produto.image) {
+            let image = document.createElement("img");
+            image.src = produto.image;
+            image.className = "imagem-produto";
+            colunaImagem.appendChild(image);
+        } else {
+            colunaImagem.innerHTML = "<b>SEM IMAGEM</b>"
+        }
+        linha.appendChild(colunaImagem); // adiciona coluna ImagemcolunaImagem
+
+        addColumn("nome", produto, linha);
+        addColumn("preco", produto, linha);
+
+        let colunaCategoria = document.createElement("td");
+        colunaCategoria.innerHTML = produto.categoria.nome;
+
+        let colunaOperacoes = document.createElement("td");
+        let id = produto.id;
+        colunaOperacoes.innerHTML = `<button onclick="transitionTo(event, '/produto/edit/${id}')"` +
+            'class="btn btn-warning">' +
+            'Editar</button>' +
+            `<button class="btn btn-danger" onclick="excluirProduto(${id})">Excluir</button>`;
+        colunaOperacoes.className = "is-logged";
+
+        linha.appendChild(colunaCategoria); // adiciona coluna categoria
+        linha.appendChild(colunaOperacoes); // adiciona coluna categoria
+        tbody.appendChild(linha);
+    });
+    verificarNodes();
+}
+
+function carregouPaginaProduto() {
+    window.addEventListener("carregoupagina", listarProdutos, { once: true });
+}
+
+function carregouFormularioProduto() {
+    window.addEventListener("carregoupagina", verificarEdicao, { once: true });
+}
+
+async function carregarForm() {
+    let select = document.getElementById("categoria_select");
+    let categorias = await getAllCategoria();
+    categorias.forEach(categoria => {
+        let option = document.createElement("option");
+        option.value = categoria.id;
+        option.innerText = categoria.nome;
+        select.appendChild(option);
+    });
+}
+
+async function verificarEdicao() {
+    await carregarForm();
+    let id = window.location.pathname.match(/\d+$/)
+    if (id) {
+        id = id[0];
+        let resposta = await sendRequest("/api/produto/" + id);
+        // fetch(urlApi + "/api/produto").then(function(resposta){ });
+        let produto = await resposta.json();
+        let inputNome = document.querySelector("input[name=nome]");
+        inputNome.value = produto.nome;
+        let inputDescricao = document.querySelector("input[name=descricao]");
+        inputDescricao.value = produto.descricao;
+        let inputPreco = document.querySelector("input[name=preco]");
+        inputPreco.value = produto.preco;
+        let inputEstoque = document.querySelector("input[name=estoque]");
+        inputEstoque.value = produto.estoque;
+        let inputImage = document.querySelector("input[name=image]");
+        inputImage.value = produto.image;
+
+        let inputCategoria = document.getElementById("categoria_select");
+        inputCategoria.value = produto.categoria.id;
+    }
+}
+
+async function saveProduto(event) {
+    event.preventDefault();
+    let form = event.target;
+    let json = formDataToJson(form);
+
+    let id = window.location.pathname.match(/\d+$/);
+    if (id) {
+        id = id[0];
+        json.id = id;
+    }
+    json.categoria = Number(json.categoria);
+    let jsonString = JSON.stringify(json);
+
+    let token = getToken();
+
+    let resposta = await sendRequest("/api/produto", {
+        method: id == null ? "POST" : "PUT",
+        body: jsonString,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    if (resposta.status == 200) {
+        alert("Produto salvo com sucesso!");
+        transitionTo(null, "/produto");
+    }
+    else {
+        if(document.querySelector("input[name=nome]").value == '') {
+            const inp = document.getElementsByClassName('Nome')[0];
+            inp.style.color = 'red';
+            inp.style.fontSize = '25';}
+
+        if(document.querySelector("input[name=descricao]").value == '') {
+            const inp = document.getElementsByClassName('Descricao')[0];
+            inp.style.color = 'red';
+            inp.style.fontSize = '25';}
+
+        if(document.querySelector("input[name=preco]").value == '') {
+            const inp = document.getElementsByClassName('Preco')[0];
+            inp.style.color = 'red';
+            inp.style.fontSize = '25';}
+
+        if(document.querySelector("input[name=estoque]").value == '') {
+            const inp = document.getElementsByClassName('Estoque')[0];
+            inp.style.color = 'red';
+            inp.style.fontSize = '25';}
+
+        if(document.querySelector("input[name=image]").value == '') {
+            const inp = document.getElementsByClassName('URL')[0];
+            inp.style.color = 'red';
+            inp.style.fontSize = '25';}
+
+    }
+}
+
+async function excluirProduto(id) {
+    let token = getToken();
+    let confirmou = confirm("Tem certeza de que deseja excluir esse registro?");
+    if (confirmou) {
+        let resposta = await sendRequest("/api/produto/" + id, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        if (resposta.status == 200) {
+            listarProdutos();
+        }
+        else {
+            alert("Não foi possível excluir o registro");
+        }
+    }
+}
---
+++ js/router.js
@@ -0,0 +1,81 @@
+function getHtml() {
+    let path = window.location.pathname;
+    switch (path) { // verifica qual é a rota
+        case "/":
+            return "/src/formulario.html";
+        case "/novaPagina":
+            return "/src/novapagina.html";
+        case "/primo":
+            return "/src/primo.html";
+        case "/categoria":
+            carregouPaginaCategoria();
+            return "/src/categoria/list.html";
+        case "/produto":
+            carregouPaginaProduto();
+            return "/src/produto/list.html";
+    }
+    if (isLoggedIn()) {
+        switch (path) {
+            case "/categoria/criar":
+                return "/src/categoria/form.html";
+            case "/produto/criar":
+                carregouFormularioProduto();
+                return "/src/produto/form.html";
+            default:
+                if (path.match(/^\/categoria\/edit\/\d+$/)) {
+                    carregarFormularioEdit();
+                    return "/src/categoria/form.html";
+                }
+                else if (path.match(/^\/produto\/edit\/\d+$/)) {
+                    carregouFormularioProduto();
+                    return "/src/produto/form.html";
+                }
+        }
+    }
+    else {
+        switch (path) {
+            case "/registrar":
+                return "/src/register.html";
+            case "/login":
+                return "/src/login.html";
+        }
+    }
+
+
+    return null;
+}
+
+
+function carregarConteudo() {
+    let html = getHtml();
+
+    let container = document.getElementById("container");
+
+    // if -> fetch() -> console.log()
+    //       http request -> resposta.text()
+    //                       serializa para texto -> container.innerHTML = texto
+    if (html) {
+        fetch(html) // Promise
+            .then(
+                // quando o servidor retornar a resposta HTTP
+                resposta => resposta.text() // Segunda Promise
+            )
+            // quando a resposta for transformada em string
+            .then(function (texto) {
+                container.innerHTML = texto;
+                window.dispatchEvent(new Event("carregoupagina"));
+            });
+    } else {
+        window.dispatchEvent(new Event("carregoupagina"));
+    }
+}
+
+function transitionTo(event, path) {
+    event && event.preventDefault(); // impede que a tag "a" ao ser clicada redirecione a pagina
+    window.history.pushState("", "", path); // troco apenas a url sem redirecionar
+    carregarConteudo();
+}
+
+window.addEventListener("popstate", () => carregarConteudo());
+
+window.addEventListener("load", () => carregarConteudo());
